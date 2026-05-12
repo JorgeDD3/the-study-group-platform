1e8e935cadbcafeccd827fb1d4e9eb3b9ab72211
Gddelp <gddelp@go.olemiss.edu>
2026-05-12 15:00:30 -0500

    Connect Clerk users to app records

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { UserRole } from "@prisma/client";
-import { getUserByClerkId } from "@/lib/users";
+import { getOrCreateAppUser } from "@/lib/users";
 
 function getDashboardPath(role: UserRole) {
   switch (role) {
@@ -24,11 +24,24 @@ export default async function DashboardPage() {
     redirect("/sign-in");
   }
 
-  const user = await getUserByClerkId(userId);
+  const clerkUser = await currentUser();
 
-  if (!user) {
+  if (!clerkUser) {
+    redirect("/sign-in");
+  }
+
+  const email = clerkUser.emailAddresses[0]?.emailAddress;
+
+  if (!email) {
     redirect("/client");
   }
+
+  const user = await getOrCreateAppUser({
+    clerkId: userId,
+    email,
+    firstName: clerkUser.firstName,
+    lastName: clerkUser.lastName,
+  });
 
   redirect(getDashboardPath(user.role));
 }
